7f1a4224e7c1f86a21911170953e29136940e192
cristian-rla <Cristianrluquea@hotmail.com>
2026-05-15 12:54:35 -0600

    updated deploy url, had extra / at the end

--- src/app/app.ts
+++ src/app/app.ts
@@ -22,7 +22,7 @@ export type AppContainer = {
 export function createApp(container: AppContainer) {
   const app = express();
 
-  const allowedOrigins = ["http://localhost:3000", "https://workhubmty-backend-production.up.railway.app/"];
+  const allowedOrigins = ["http://localhost:3000", "https://workhubmty-backend-production.up.railway.app"];
 
   app.use(cors({
     origin: ( origin, callback ) => {
